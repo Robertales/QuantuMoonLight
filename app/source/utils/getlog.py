0f43003907107f795a00eddfc3fcbb39fd2fa6c7
CicaMatt <matteocicalese01@gmail.com>
2021-12-30 17:07:15 +0100

    il file di log ora è log.txt e non log,txt cambiamento fatto anche nel percorso presente in getlog.py

--- app/source/utils/getlog.py
+++ app/source/utils/getlog.py
@@ -11,7 +11,7 @@ print("\n", dt_string)
 print("\n")
 ROOT_DIR = Path(__file__).parents[2].__str__()
 
-file1 = open(ROOT_DIR+"\\log\\log,txt ", "a+")
+file1 = open(ROOT_DIR+"\\log\\log.txt ", "a+")
 
 file1.write(dt_string + "\n")
 file1.close()
