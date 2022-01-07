d7c175e632b2094de0b63f62acce10723ea0107a
Lucacontr <lucacontrasto@gmail.com>
2022-01-08 00:06:34 +0100

    Added pydoc and testing for classification

--- app/source/classificazioneDataset/test_ClassificazioneControl.py
+++ app/source/classificazioneDataset/test_ClassificazioneControl.py
@@ -19,14 +19,9 @@ class TestClassificazioneControl(unittest.TestCase):
         pathTrain = pathlib.Path(__file__).cwd() / "testingFiles" / "DataSetTrainPreprocessato.csv"
         pathTest = pathlib.Path(__file__).cwd() / "testingFiles" / "DataSetTestPreprocessato.csv"
         pathPrediction = pathlib.Path(__file__).cwd() / "testingFiles" / "doPrediction.csv"
-        numCols = utils.numberOfColumns(pathTrain)
-        features = utils.createFeatureList(numCols - 1)
+        features = utils.createFeatureList(2)
         token = '43a75c20e78cef978267a3bdcdb0207dab62575c3c9da494a1cd344022abc8a326ca1a9b7ee3f533bb7ead73a5f9fe519691a7ad17643eecbe13d1c8c4adccd2'
         backend = "ibmq_qasm_simulator"
-
-        #fileTrain = open(pathTrain, "r")
-        #fileTest = open(pathTest, "r")
-        #filePrediction = open(pathPrediction, "r")
 
         response = tester.post(
             '/classificazioneControl',
@@ -34,7 +29,5 @@ class TestClassificazioneControl(unittest.TestCase):
                       userpathToPredict=pathPrediction, features=features,
                       token=token, backend=backend))
         statuscode = response.status_code
-        result = response.get_data()
         self.assertEqual(statuscode, 200)
-        self.assertTrue(exists("upload_dataset" / "classifiedFile.csv")
-)
+        self.assertTrue(exists("C:\\Users\\lucac\\PycharmProjects\\QuantuMoonLight\\upload_dataset\\classifiedFile.csv"))
