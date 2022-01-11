d368a9f6e0b1aa1926f8e92c1abc27664d83c8ae
Fiorentino Tomeo <fiore.tom94@gmail.com>
2022-01-11 19:17:59 +0100

    Documentation for functions in utils.py

--- app/source/utils/utils.py
+++ app/source/utils/utils.py
@@ -6,8 +6,14 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 
 
-# ritorna il numero di colonne di un dataset (di default conta anche la colonna dei labels)
 def numberOfColumns(filename):
+    """
+    This function return the number of columns of a given dataset
+
+    :param filename: name of the dataset
+    :return: number of columns of the given dataset
+    :rtype: int
+    """
     f = open(filename, 'r')
     reader = csv.reader(f, delimiter=',')
     numCols = len(next(reader))
@@ -15,15 +21,28 @@ def numberOfColumns(filename):
     return numCols  # Read first line and count columns
 
 
-# ritorna il numero di righe di un dataset
 def numberOfRows(filename):
+    """
+    This function return the number of raws of a given dataset
+
+    :param filename: name of the dataset
+    :return: number of raws of the given dataset
+    :rtype: int
+    """
     results = pd.read_csv(filename)
 
     return len(results)
 
 
-# crea la Lista contenente le label delle feature
-def createFeatureList(numCols):
+def createFeatureList(numCols: int):
+    """
+    This function create a List of string such as ["feature1","feature2",...,"featureN"
+    where N is given in input
+
+    :param numCols: number of feature to insert in the list
+    :return: List of feature
+    :rtype: list
+    """
     featureList = []
     for x in range(numCols):
         stringa = "feature{}".format(x + 1)
@@ -70,13 +89,32 @@ def prepareData(databasePath):
 
 
 def writeTxt(fileName, list_values):
+    """
+    This function write in a file .txt the values stored in a list
+
+    :param fileName: name of the output file
+    :param list_values: list of values to write
+    :return: name of the output file
+    :rtype: str
+    """
     f = open(fileName, "w+")
     for c in list_values:
         f.write(str(c) + "\n")
     f.close()
+    return fileName
 
 
 def writeXls(fileName, generations, evaluations, bestfits, times):
+    """
+    This function is used in Prototype Selection to write important info of the Genetic algorithm in a file .xls
+
+    :param fileName: name of the output file
+    :param generations:
+    :param evaluations:
+    :param bestfits:
+    :param times:
+    :return: name of the output file
+    """
     wb = Workbook()
     ws1 = wb.active
 
@@ -107,4 +145,6 @@ def writeXls(fileName, generations, evaluations, bestfits, times):
 
     wb.save(filename=fileName)
     wb.close()
+
+    return fileName
 
